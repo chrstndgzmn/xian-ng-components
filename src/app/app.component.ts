12b7dd31da9ed693218541491517f6fa8313b5e8
xian <christiandeguzman>
2024-09-11 13:50:24 -0600

    init angular from scratch

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'app-root',
+    // templateUrl: 'app.component.html', 
+    template: '<h1>Welcome to my playground</h1>',
+    templateUrl:'./app.component.html',
+    styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+    title = "app component"
+}
